c83f695b1629c6b04daa83840f286b29b4141711
Zachary Marszale <zmarszal@ameritech.net>
2019-01-23 14:53:33 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hi from JS')
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiem1hcnN6YWwiLCJhIjoiY2pyOW5ud25zMGphNjQzbGluanB5b3Y3ayJ9.Mm92jc4dS81FaLIePR3cuA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
